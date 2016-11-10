644ad418956c4d3072dfd0e7a3964adae32e6d8f
ashrafuli <ashrafulkhan68@gmail.com>
2016-11-10 10:10:22 +0530

    it is C program

---
+++ ao.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int a=6,b=2;
+  int sum,div,mul,def;
+  sum=a+b;
+  printf("addition of two no:%d",sum);
+  div=a/b;
+  printf("division of two no:%d",div);
+  mul=a*b;
+  printf("addition of two no:%d",mul);
+  def=a-b;
+  printf("addition of two no:%d",def);
+  return 0;
+}
